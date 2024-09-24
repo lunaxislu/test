0659706f2575c217da6ffd109559a0b10ee3e552
lunaxislu <cmk0905@naver.com>
2024-09-25 03:08:45 +0900

    Link add

--- src/pages/shop/[[...id]].tsx
+++ src/pages/shop/[[...id]].tsx
@@ -4,16 +4,51 @@ import { Separator } from "@/components/ui/separator";
 import { Switch } from "@/components/ui/switch";
 import { Toggle } from "@/components/ui/toggle";
 import { GetServerSidePropsContext } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 const BASE_PATH = "/shop";
-const DynamicPages = ({ url }: { url: string }) => {
+const DynamicPages = ({ url }: { url: string | undefined }) => {
   const router = useRouter();
-  const { asPath, query } = router;
+  const [state, setState] = useState<string>("");
 
   return (
     <div className="text-yellow-600">
-      {BASE_PATH === asPath ? (
+      {state}
+      <ul className="p-4 flex gap-5 bg-orange-500 text-gray-50">
+        <Link href={"/shop"}>shop</Link>
+        <Link href={"/shop/1"} shallow={true}>
+          shop/1
+        </Link>
+        <Link href={"/shop/2"} shallow={true}>
+          shop/2
+        </Link>
+        <Link href={"/shop/3"} shallow={true}>
+          shop/3
+        </Link>
+        <Link href={"/shop/4"} shallow={true}>
+          shop/4
+        </Link>
+        <Link href={"/shop/5"} shallow={true}>
+          shop/5
+        </Link>
+        <Link href={"/shop/a/1"} shallow={true}>
+          shop/a/1
+        </Link>
+        <Link href={"/shop/a/2"} shallow={true}>
+          shop/a/2
+        </Link>
+        <Link href={"/shop/a/3"} shallow={true}>
+          shop/a/3
+        </Link>
+        <Link href={"/shop/a/4"} shallow={true}>
+          shop/a/4
+        </Link>
+        <Link href={"/shop/a/5"} shallow={true}>
+          shop/a/5
+        </Link>
+      </ul>
+      {BASE_PATH === router.asPath ? (
         <div>
           <Calendar />
           <Calendar />
@@ -105,7 +140,6 @@ const DynamicPages = ({ url }: { url: string }) => {
 export default DynamicPages;
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  console.log(ctx.req.url);
   return {
     props: {
       url: ctx.req.url,
--- src/pages/test/[[...id]].tsx
+++ src/pages/test/[[...id]].tsx
@@ -1,18 +1,58 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Calendar } from "@/components/ui/calendar";
 import { Separator } from "@/components/ui/separator";
 import { Switch } from "@/components/ui/switch";
 import { Toggle } from "@/components/ui/toggle";
 import { GetServerSidePropsContext } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 const BASE_PATH = "/shop";
-const DynamicPageCompare = ({ url }: { url: string }) => {
+const DynamicPageCompare = ({ url }: { url: string | undefined }) => {
   const router = useRouter();
-  const { asPath, query } = router;
+  const [state, setState] = useState<string>("");
 
+  useEffect(() => {
+    setState(router.asPath);
+  }, [router.asPath]);
   return (
     <div className="text-yellow-600">
+      {state}
+
+      <ul className="p-4 flex gap-5 bg-orange-500 text-gray-50">
+        <Link href={"/test"}>test</Link>
+        <Link href={"/test/1"} shallow={true}>
+          test/1
+        </Link>
+        <Link href={"/test/2"} shallow={true}>
+          test/2
+        </Link>
+        <Link href={"/test/3"} shallow={true}>
+          test/3
+        </Link>
+        <Link href={"/test/4"} shallow={true}>
+          test/4
+        </Link>
+        <Link href={"/test/5"} shallow={true}>
+          test/5
+        </Link>
+        <Link href={"/test/a/1"} shallow={true}>
+          test/a/1
+        </Link>
+        <Link href={"/test/a/2"} shallow={true}>
+          test/a/2
+        </Link>
+        <Link href={"/test/a/3"} shallow={true}>
+          test/a/3
+        </Link>
+        <Link href={"/test/a/4"} shallow={true}>
+          test/a/4
+        </Link>
+        <Link href={"/test/a/5"} shallow={true}>
+          test/a/5
+        </Link>
+      </ul>
       <Calendar />
       <Calendar />
       <Calendar />
@@ -59,10 +99,10 @@ const DynamicPageCompare = ({ url }: { url: string }) => {
 export default DynamicPageCompare;
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  console.log(ctx.req.url);
   return {
     props: {
       url: ctx.req.url,
+      isSSR: true,
     },
   };
 };
